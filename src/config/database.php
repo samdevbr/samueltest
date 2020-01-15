b94f297af804e8aacbb678d3c54b2fd1d2c624fc
Samuel Oliveira <samuel.oliveira@getrak.com.br>
2020-01-15 15:17:54 -0300

    Configuring mongodb

--- src/config/database.php
+++ src/config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'mongodb'),
 
     /*
     |--------------------------------------------------------------------------
@@ -34,6 +34,14 @@ return [
     */
 
     'connections' => [
+        'mongodb' => [
+            'driver'   => 'mongodb',
+            'host'     => env('DB_HOST', 'localhost'),
+            'port'     => env('DB_PORT', 27017),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+        ],
 
         'sqlite' => [
             'driver' => 'sqlite',

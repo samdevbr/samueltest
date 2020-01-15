44df24df05e181bd8aa2dba0d16629a566b71afb
Samuel Oliveira <samuel.oliveira@getrak.com.br>
2020-01-15 18:27:38 -0300

    Creating docs for users endpoint

---
+++ src/app/Docs/Info.php
@@ -0,0 +1,10 @@
+<?php
+namespace App;
+
+/**
+ * @OA\Info(
+ *     description="",
+ *     version="3.0.0",
+ *     title="Samuel Teste Api Docs",
+ * )
+ */
--- src/app/Http/Controllers/UsersController.php
+++ src/app/Http/Controllers/UsersController.php
@@ -20,11 +20,72 @@ class UsersController
 		$this->userService = $userService;
 	}
 
+	/**
+     * @OA\Get(
+     *     path="/users",
+     *     summary="Returns all users.",
+     *     operationId="onList",
+     *     @OA\Response(
+     *        response=200,
+     *        description="Returns all users.",
+     *        @OA\JsonContent(
+     *          type="array",
+     *          @OA\Items(properties={
+	 * 				@OA\Property(property="_id", type="string"),
+	 * 				@OA\Property(property="name", type="string"),
+	 * 				@OA\Property(property="email", type="string"),
+	 * 				@OA\Property(property="updated_at", type="string"),
+	 * 				@OA\Property(property="created_at", type="string"),
+	 * 			})
+     *        )
+     *     )
+     * )
+     */
 	protected function onList(Request $request)
 	{
 		return $this->userService->all();
 	}
 
+	/**
+     * @OA\Get(
+     *     path="/users/{id}",
+	 *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="User id",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *     summary="Returns an specific user by it's id.",
+     *     operationId="onGet",
+     *     @OA\Response(
+     *        response=200,
+     *        description="Returns an specific user by it's id.",
+     *        @OA\JsonContent(
+     *          type="array",
+     *          @OA\Items(properties={
+	 * 				@OA\Property(property="_id", type="string"),
+	 * 				@OA\Property(property="name", type="string"),
+	 * 				@OA\Property(property="email", type="string"),
+	 * 				@OA\Property(property="updated_at", type="string"),
+	 * 				@OA\Property(property="created_at", type="string"),
+	 * 			})
+     *        )
+     *     ),
+	 *     @OA\Response(
+	 * 	       response=404,
+	 * 		   description="User not found",
+	 * 		   @OA\JsonContent(
+	 *		     type="object",
+	 *           properties={
+	 *              @OA\Property(property="error", type="string")
+	 *           }
+	 *         )
+	 * 	   )
+     * )
+     */
 	protected function onGet(Request $request, $identifer)
 	{
 		return $this->userService->getById($identifer);
@@ -38,6 +99,48 @@ class UsersController
 		]);
 	}
 
+     /**
+     * @OA\Post(
+     *     path="/users",
+     *     summary="Creates an user.",
+     *     operationId="onCreate",
+	 *     requestBody=@OA\RequestBody(
+	 *     	   required=true,
+	 *         request="Create user",
+	 *         @OA\JsonContent(
+	 *             type="object",
+	 *             properties={
+	 *                 @OA\Property(property="name", type="string"),
+	 * 				   @OA\Property(property="email", type="string")
+	 *             } 
+	 *         )
+	 *     ),
+     *     @OA\Response(
+     *        response=200,
+     *        description="Creates an user.",
+     *        @OA\JsonContent(
+     *          type="object",
+     *          properties={
+	 * 				@OA\Property(property="_id", type="string"),
+	 * 				@OA\Property(property="name", type="string"),
+	 * 				@OA\Property(property="email", type="string"),
+	 * 				@OA\Property(property="updated_at", type="string"),
+	 * 				@OA\Property(property="created_at", type="string"),
+	 * 			}
+     *        )
+     *     ),
+	 *     @OA\Response(
+	 * 	       response=422,
+	 * 		   description="Request payload not valid",
+	 * 		   @OA\JsonContent(
+	 *		     type="object",
+	 *           properties={
+	 *              @OA\Property(property="error", type="string")
+	 *           }
+	 *         )
+	 * 	   )
+     * )
+     */
 	protected function onCreate(Request $request)
 	{
 		return $this->userService->insert($request->all());
@@ -51,11 +154,106 @@ class UsersController
 		]);
 	}
 
+     /**
+     * @OA\Put(
+     *     path="/users/{id}",
+	 * 	   @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="User id",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *     summary="Updates a user.",
+     *     operationId="onUpdate",
+	 *     requestBody=@OA\RequestBody(
+	 *     	   required=true,
+	 *         request="Update user",
+	 *         @OA\JsonContent(
+	 *             type="object",
+	 *             properties={
+	 *                 @OA\Property(property="name", type="string"),
+	 * 				   @OA\Property(property="email", type="string")
+	 *             } 
+	 *         )
+	 *     ),
+     *     @OA\Response(
+     *        response=200,
+     *        description="User updated.",
+     *        @OA\JsonContent(
+     *          type="object",
+     *          properties={
+	 * 				@OA\Property(property="_id", type="string"),
+	 * 				@OA\Property(property="name", type="string"),
+	 * 				@OA\Property(property="email", type="string"),
+	 * 				@OA\Property(property="updated_at", type="string"),
+	 * 				@OA\Property(property="created_at", type="string"),
+	 * 			}
+     *        )
+     *     ),
+	 *     @OA\Response(
+	 * 	       response=422,
+	 * 		   description="Request payload not valid",
+	 * 		   @OA\JsonContent(
+	 *		     type="object",
+	 *           properties={
+	 *              @OA\Property(property="error", type="string")
+	 *           }
+	 *         )
+	 * 	   ),
+	 * 	   @OA\Response(
+	 * 	       response=404,
+	 * 		   description="User not found",
+	 * 		   @OA\JsonContent(
+	 *		     type="object",
+	 *           properties={
+	 *              @OA\Property(property="error", type="string")
+	 *           }
+	 *         )
+	 * 	   )
+     * )
+     */
 	protected function onUpdate(Request $request, $identifer)
 	{
 		return $this->userService->update($identifer, $request->all());
 	}
 
+     /**
+     * @OA\Delete(
+     *     path="/users/{id}",
+	 * 	   @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="User id",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *     summary="Deletes a user.",
+     *     operationId="onDelete",
+     *     @OA\Response(
+     *        response=200,
+     *        description="User deleted.",
+     *        @OA\JsonContent(
+     *          type="object",
+     *          properties={}
+     *        )
+     *     ),
+	 * 	   @OA\Response(
+	 * 	       response=404,
+	 * 		   description="User not found",
+	 * 		   @OA\JsonContent(
+	 *		     type="object",
+	 *           properties={
+	 *              @OA\Property(property="error", type="string")
+	 *           }
+	 *         )
+	 * 	   )
+     * )
+     */
 	protected function onDelete(Request $request, $identifer)
 	{
 		$this->userService->delete($identifer);
